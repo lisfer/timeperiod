a17a9e5d545a4b4ba0d5ad000d56f7ca9fed4bb0
lisfer <makrushinds@gmail.com>
2018-12-05 12:40:10 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='timeperiod',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     description='Simple date parser',
     long_description=open(join(dirname(__file__), 'README.md')).read(),
